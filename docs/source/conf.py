160d43ff53876ad3c6845539a35604cc77ae36db
amerotz <amerotz@proton.me>
2023-06-26 17:56:36 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'LOERIC'
+copyright = '2023, Marco Amerotti'
+author = 'Marco Amerotti'
 
 release = '0.1'
 version = '0.1.0'
