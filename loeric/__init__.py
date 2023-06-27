06c002853a2a6f80c20253e7c18f15f8335549a3
Marco Amerotti <marco@sam.lan>
2023-06-27 17:52:32 +0200

    changed version place

--- loeric/__init__.py
+++ loeric/__init__.py
@@ -1,5 +1,3 @@
 """
 LOERIC - a live performance rule system for Irish traditional music.
 """
-
-__version__ = "0.1.0"
--- loeric/loeric.py
+++ loeric/loeric.py
@@ -1,3 +1,5 @@
+__version__ = "0.1.0"
+
 import mido
 import matplotlib.pyplot as plt
 
