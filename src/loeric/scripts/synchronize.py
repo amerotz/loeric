b5aa0e803a8d1ca29f4d7e9b062bd92e5f28391a
Marco Amerotti <marco@fragile.com>
2025-01-14 13:05:28 +0100

    shell script polish

--- src/loeric/scripts/synchronize.py
+++ src/loeric/scripts/synchronize.py
@@ -13,16 +13,12 @@ last_tempo = 120
 fix_sync_duration = 0
 stop_sync_duration = 0
 songpos_wait = 0
-min_window_size = 0.1
-max_window_size = 0.25
-window_size = max_window_size
 exiting = threading.Event()
-increase_window = threading.Event()
-sync_thread = None
 switch_timer = 0
+sync_thread = None
 intensity_thread = None
 port_lock = threading.Lock()
-window_lock = threading.Lock()
+all_dead = threading.Semaphore(value=2)
 
 
 def send_tempo(tempo, port):
@@ -36,7 +32,7 @@ def send_tempo(tempo, port):
 
 
 def update_tempo(tempo):
-    global last_tempo, stop_sync_duration, fix_sync_duration, songpos_wait, window_size, window_lock, switch_timer
+    global last_tempo, stop_sync_duration, fix_sync_duration, songpos_wait, switch_timer
     last_tempo = tempo
     fix_sync_duration = 0.0625 * 60 / last_tempo
     stop_sync_duration = 0.5 * 60 / last_tempo
@@ -47,7 +43,7 @@ def update_tempo(tempo):
 def sync_intensity(inports, outports):
     global exiting, switch_timer, all_dead
     all_dead.acquire()
-    print("Intensity Sync ON.")
+    shell_print("Intensity Sync ON.")
     int_dict = {}
     action_dict = {}
     while not exiting.is_set():
@@ -116,7 +112,7 @@ def sync_intensity(inports, outports):
             value = min(value, 127)
             value = max(value, 0)
 
-            # print(loeric_id, value, out_port)
+            # shell_print(loeric_id, value, out_port)
             # prepare message
             if out_port is not None:
                 msg = mido.Message("control_change", value=value, control=42)
@@ -126,13 +122,13 @@ def sync_intensity(inports, outports):
             traceback.print_exception(e)
             exiting.set()
     all_dead.release()
-    print("Intensity Sync OFF.")
+    shell_print("Intensity Sync OFF.")
 
 
 def sync_loeric(inports, outports):
-    global stop_sync_duration, fix_sync_duration, exiting, min_window_size, window_size, last_tempo, num_clocks, all_dead
+    global stop_sync_duration, fix_sync_duration, exiting, last_tempo, num_clocks, all_dead, songpos_wait
     all_dead.acquire()
-    print("\nSync ON.")
+    shell_print("Sync ON.")
     pos_dict = {}
     updated_dict = defaultdict(bool)
     sleepers = []
@@ -152,9 +148,9 @@ def sync_loeric(inports, outports):
                     with port_lock:
                         pos_dict[lid] = (current, pos)
                         port.send(mido.Message("continue"))
-                        # print(re.search("#.*#", port.name)[0])
-                        # print(f"{lid}: AWAKEN at {pos}")
-                        # print()
+                        # shell_print(re.search("#.*#", port.name)[0])
+                        # shell_print(f"{lid}: AWAKEN at {pos}")
+                        # shell_print()
                 else:
                     # put them back
                     new_sleepers.append(s)
@@ -174,7 +170,7 @@ def sync_loeric(inports, outports):
 
             # who sent this?
             loeric_id = re.search("#.*#", port.name)[0]
-            # print(f"{loeric_id}: SENT {msg.pos} ({now})")
+            # shell_print(f"{loeric_id}: SENT {msg.pos} ({now})")
 
             # check what position we should consider
             # store a tuple (time, position) for each
@@ -182,7 +178,7 @@ def sync_loeric(inports, outports):
 
             # agree on which position
             pos = max([t[1] for t in pos_dict.values()])
-            # print(f"SYNC {pos}")
+            # shell_print(f"SYNC {pos}")
 
             # agree on what time
             timestamp = np.mean([t[0] for t in pos_dict.values() if t[1] == pos])
@@ -211,17 +207,13 @@ def sync_loeric(inports, outports):
             # stop and continue from next beat
             if diff >= stop_thr or msg.pos != pos:
 
-                """
-                print(round(diff, 4), round(stop_thr, 4))
-                print("stopping")
-                """
                 with port_lock:
                     # tell port to wait
                     out_port.send(mido.Message("stop"))
                     # tell port to start at next songpos
                     out_port.send(mido.Message("songpos", pos=pos + 1))
 
-                # print(f"{loeric_id}: SLEEP at {pos}")
+                # shell_print(f"{loeric_id}: SLEEP at {pos}")
 
                 sleepers.append(
                     (loeric_id, now, songpos_wait - diff, out_port, pos + 1)
@@ -230,10 +222,6 @@ def sync_loeric(inports, outports):
             # soft fix
             # send a tempo bump
             elif diff >= fix_thr:
-                """
-                print(round(diff, 4), round(fix_thr, 4))
-                print("fixing")
-                """
                 # calculate the new tempo
                 # so that we synchronize on the next beat
                 q0 = timestamp + songpos_wait
@@ -247,142 +235,229 @@ def sync_loeric(inports, outports):
                     send_tempo(new_tempo, out_port)
                 updated_dict[loeric_id] = True
 
-            # print("---------")
+            # shell_print("---------")
 
         except Exception as e:
             traceback.print_exception(e)
             exiting.set()
     all_dead.release()
-    print("\nSync OFF.")
+    shell_print("Sync OFF.")
+
+
+def shell_print(s):
+    print(s)
+    print("\033[1m\033[92;40mLOERIC >\033[0m", end=" ")
+
+
+def close_shell():
+    global exiting, all_dead
+    exiting.set()
+    all_dead.acquire()
+    all_dead.acquire()
+    shell_print("Terminating.")
+
+
+def check_args(command, num_args=0, values=[], optional=True):
+    args = command.split(" ")
+    if len(args) > num_args + 1 or (not optional and len(args) != num_args + 1):
+        return False
+    if optional and len(args) == 1:
+        return True
+
+    if len(values) != 0:
+        for a in args[1:]:
+            if not a in values:
+                return False
+    return True
 
 
 def main():
     update_tempo(last_tempo)
     sync_thread = threading.Thread()
     intensity_thread = threading.Thread()
-    all_dead = threading.Semaphore(value=2)
+    sync_ports_in = []
+    sync_ports_out = []
+    intensity_ports_in = []
+    intensity_ports_out = []
     multi_out = None
-    while True:
-        command = input("\033[1m\033[92;40mLOERIC >\033[0m ")
-        # connect to LOERIC ports
-        if command.startswith("connect"):
-            arg = command.split(" ")[-1]
+    print("\033[1m\033[92;40mLOERIC >\033[0m", end=" ")
+    try:
+        while True:
+            command = input()
+            arg_0 = command.split(" ")[0]
+            # connect to LOERIC ports
+            if arg_0 == "connect":
+                if not check_args(
+                    command, num_args=1, values=["in", "out"], optional=True
+                ):
+                    shell_print("Invalid argument.")
+                    exiting.set()
+                    all_dead.acquire()
+                    all_dead.acquire()
+                    exiting.clear()
+                    all_dead.release(n=2)
 
-            if arg == "in" or arg == "connect":
-                # ports to input messages from loeric
-                intensity_ports_in = [
-                    mido.open_input(p)
-                    for p in mido.get_input_names()
-                    if "LOERIC out" in p or "HUMAN out" in p
-                ]
-                if len(intensity_ports_in) == 0:
-                    print("No INT input to connect to.")
                 else:
-                    print("Opening ports", intensity_ports_in)
-                    print("LOERIC INT 2 Shell connected.")
+                    arg = command.split(" ")[-1]
+                    s = []
 
-                # ports to input messages from loeric
-                sync_ports_in = [
-                    mido.open_input(p)
-                    for p in mido.get_input_names()
-                    if "LOERIC SYNC" in p
-                ]
-                if len(sync_ports_in) == 0:
-                    print("No SYNC input to connect to.")
+                    if arg == "in" or arg == "connect":
+                        # ports to input messages from loeric
+                        intensity_ports_in = [
+                            mido.open_input(p)
+                            for p in mido.get_input_names()
+                            if "LOERIC out" in p or "HUMAN out" in p
+                        ]
+                        if len(intensity_ports_in) == 0:
+                            s.append("No INT input to connect to.")
+                        else:
+                            s.append(
+                                f"Opening ports:\n{intensity_ports_in}\nLOERIC INT 2 Shell connected."
+                            )
+
+                        # ports to input messages from loeric
+                        sync_ports_in = [
+                            mido.open_input(p)
+                            for p in mido.get_input_names()
+                            if "LOERIC SYNC" in p
+                        ]
+                        if len(sync_ports_in) == 0:
+                            s.append("No SYNC input to connect to.")
+                        else:
+                            s.append(
+                                f"Opening ports:\n{sync_ports_in}\nLOERIC SYNC 2 Shell connected."
+                            )
+
+                    if arg == "out" or arg == "connect":
+                        intensity_ports_out = [
+                            mido.open_output(p)
+                            for p in mido.get_output_names()
+                            if "LOERIC in" in p or "HUMAN in" in p
+                        ]
+                        if len(intensity_ports_out) == 0:
+                            s.append("No INT output to connect to.")
+                        else:
+                            s.append(
+                                f"Opening ports:\n{intensity_ports_out}\nShell 2 LOERIC INT connected."
+                            )
+
+                        # ports to output messages to loeric
+                        sync_ports_out = [
+                            mido.open_output(p)
+                            for p in mido.get_output_names()
+                            if "LOERIC SYNC" in p
+                        ]
+                        if len(sync_ports_out) == 0:
+                            s.append("No output to connect to.")
+                        else:
+                            multi_out = mido.ports.MultiPort(sync_ports_out)
+                            s.append(
+                                f"Opening ports:\n{sync_ports_out}\nShell 2 LOERIC SYNC connected."
+                            )
+
+                    s = "\n".join(s)
+                    shell_print(s)
+
+            # list ports
+            elif arg_0 == "list":
+                if not check_args(
+                    command, num_args=1, values=["in", "out"], optional=True
+                ):
+                    shell_print("Invalid argument.")
                 else:
-                    print("Opening ports", sync_ports_in)
-                    print("LOERIC SYNC 2 Shell connected.")
+                    arg = command.split(" ")[-1]
+                    s = ""
+                    if arg == "in" or arg == "list":
+                        s += f"Inputs:\n{mido.get_input_names()}"
+                    if arg == "out" or arg == "list":
+                        if len(s) != 0:
+                            s += "\n"
+                        s += f"Outputs:\n{mido.get_output_names()}"
+                    shell_print(s)
+            # start and stop
+            elif arg_0 in ["sync", "start", "stop", "continue"]:
+                # start syncing
+                s = []
+                if command in ["start", "sync"]:
+                    # kill sync thread
+                    # this also kills the window thread
+                    exiting.set()
+                    all_dead.acquire()
+                    all_dead.acquire()
+                    exiting.clear()
+                    all_dead.release(n=2)
+                    if len(sync_ports_in) != 0 and len(sync_ports_out) != 0:
+                        # create and start thread
+                        sync_thread = threading.Thread(
+                            target=sync_loeric, args=([sync_ports_in, sync_ports_out])
+                        )
+                        sync_thread.start()
+                    else:
+                        s.append("Could not start SYNC thread.")
+                    if len(intensity_ports_in) != 0 and len(intensity_ports_out) != 0:
+                        intensity_thread = threading.Thread(
+                            target=sync_intensity,
+                            args=([intensity_ports_in, intensity_ports_out]),
+                        )
+                        intensity_thread.start()
+                    else:
+                        s.append("Could not start INT thread.")
 
-            if arg == "out" or arg == "connect":
-                intensity_ports_out = [
-                    mido.open_output(p)
-                    for p in mido.get_output_names()
-                    if "LOERIC in" in p or "HUMAN in" in p
-                ]
-                if len(intensity_ports_out) == 0:
-                    print("No INT output to connect to.")
+                    # reset termination flag
+                if command != "sync" and multi_out is not None:
+                    # send message
+                    msg = mido.Message(command)
+                    multi_out.send(msg)
+                    s.append(str(msg))
                 else:
-                    print("Opening ports", intensity_ports_out)
-                    print("Shell 2 LOERIC INT connected.")
-
-                # ports to output messages to loeric
-                sync_ports_out = [
-                    mido.open_output(p)
-                    for p in mido.get_output_names()
-                    if "LOERIC SYNC" in p
-                ]
-                if len(sync_ports_out) == 0:
-                    print("No output to connect to.")
+                    s.append("Could not send command. Please connect to LOERIC first.")
+                s = "\n".join(s)
+                shell_print(s)
+            # song position
+            elif arg_0 == "songpos":
+                if not check_args(command, num_args=1, values=[], optional=False):
+                    shell_print("Invalid argument.")
                 else:
-                    print("Opening ports", sync_ports_out)
-                    multi_out = mido.ports.MultiPort(sync_ports_out)
-                    print("Shell 2 LOERIC SYNC connected.")
-
-            if arg not in ["in", "out", "connect"]:
-                print("Could not parse command.")
+                    if multi_out is not None:
+                        cmd = "songpos"
+                        try:
+                            val = int(command.split(" ")[-1])
+                            msg = mido.Message(cmd, pos=val)
+                            multi_out.send(msg)
+                            shell_print(msg)
+                        except:
+                            shell_print("Invalid argument.")
+                    else:
+                        shell_print("Please connect to LOERIC first.")
+            # close shell
+            elif arg_0 == "exit":
+                close_shell()
+                break
+            # tempo change
+            elif arg_0 == "tempo":
+                if not check_args(
+                    command,
+                    num_args=1,
+                    values=[str(n) for n in range(20, 301)],
+                    optional=False,
+                ):
+                    shell_print(
+                        "Invalid argument. Tempo value must be an integer between 20 and 300."
+                    )
+                else:
+                    # get tempo
+                    val = int(command.split(" ")[-1])
+                    # update tempo info
+                    update_tempo(val)
+                    if sync_ports_out is not None:
+                        for p in sync_ports_out:
+                            send_tempo(val, p)
+                    shell_print(f"Tempo set to {val}.")
             else:
-                exiting.set()
-                all_dead.acquire()
-                all_dead.acquire()
-                exiting.clear()
-                all_dead.release(n=2)
-
-        # list ports
-        elif command == "list":
-            arg = command.split(" ")[-1]
-            if arg == "in" or arg == "list":
-                print("Inputs:")
-                print(mido.get_input_names())
-            if arg == "out" or arg == "list":
-                print("Outputs:")
-                print(mido.get_output_names())
-        # start and stop
-        elif command in ["sync", "start", "stop", "continue"]:
-            # start syncing
-            if command in ["start", "sync"]:
-                # kill sync thread
-                # this also kills the window thread
-                exiting.set()
-                all_dead.acquire()
-                all_dead.acquire()
-                exiting.clear()
-                all_dead.release(n=2)
-                # create and start thread
-                sync_thread = threading.Thread(
-                    target=sync_loeric, args=([sync_ports_in, sync_ports_out])
-                )
-                sync_thread.start()
-                intensity_thread = threading.Thread(
-                    target=sync_intensity,
-                    args=([intensity_ports_in, intensity_ports_out]),
-                )
-                intensity_thread.start()
-                # reset termination flag
-            if command != "sync" and multi_out is not None:
-                # send message
-                msg = mido.Message(command)
-                multi_out.send(msg)
-                print(msg)
-        # song position
-        elif command.startswith("songpos"):
-            cmd = "songpos"
-            val = int(command.split(" ")[-1])
-            msg = mido.Message(cmd, pos=val)
-            multi_out.send(msg)
-            print(msg)
-        # close shell
-        elif command == "exit":
-            exiting.set()
-            print("Terminating.")
-            break
-        # tempo change
-        elif command.startswith("tempo"):
-            # get tempo
-            val = int(command.split(" ")[-1])
-            # update tempo info
-            update_tempo(val)
-            for p in sync_ports_out:
-                send_tempo(val, p)
-
+                shell_print("Could not parse command.")
+    except Exception as e:
+        if type(e) is KeyboardInterrupt:
+            close_shell()
         else:
-            print("Could not parse command.")
+            traceback.print_exception(e)
+            close_shell()
