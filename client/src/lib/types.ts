9c8b225c7bbdf93c0a511c2a6b4ccddfec81147a
Kevin Glover <kevin.glover@nottingham.ac.uk>
2025-10-10 14:40:24 +0100

    Commit lib/types

---
+++ client/src/lib/types.ts
@@ -0,0 +1,42 @@
+export interface Musician {
+	id: string,
+	name: string,
+	config: string,
+	midiIn: string,
+	midiOut: string,
+	instrument: string,
+	controls: {
+		name: string,
+		control: number,
+		value: number
+	}[]
+}
+
+export interface Options {
+	'trackList': string[],
+	'outputs': string[],
+	'inputs': string[],
+	'instruments': string[],
+	'audio': { [id: string]: number }
+}
+
+export interface Track {
+	'name': string,
+	'time': string,
+	'key': string,
+	'tempo': number,
+	'config': any
+}
+
+export enum LoericPlayingState {
+	STOPPED = 'STOPPED',
+	PAUSED = 'PAUSED',
+	PLAYING = 'PLAYING',
+}
+
+export interface LoericState {
+	'track': Track,
+	'state': LoericPlayingState,
+	'options': Options
+	'musicians': Musician[],
+}
