def111558a6ab3b83e1108b044f2767e4757c21b
Marco Amerotti <marco@fragile>
2026-02-17 17:35:32 +0100

    windows stderr

--- build.sh
+++ build.sh
@@ -41,30 +41,19 @@ echo "Building LOERIC CLI"
 python -m build --no-isolation --wheel
 pip install dist/loeric-2.0.0-py2.py3-none-any.whl --force-reinstall
 
-# detect os
-OS="$(uname -s)"
-case "$OS" in
-  Linux*)   PLATFORM="linux"; ADD_DATA_SEP=":"; EXT="";;
-  Darwin*)  PLATFORM="macos"; ADD_DATA_SEP=":"; EXT="";;
-  MINGW*|MSYS*|CYGWIN*|Windows*) PLATFORM="windows"; ADD_DATA_SEP=";"; EXT=".exe";;
-  *) echo "Unsupported OS: $OS"; exit 1;;
-esac
-
-echo "Detected platform: $PLATFORM"
-
 # pyinstaller
 echo "Building LOERIC executable with PyInstaller..."
 PYINSTALLER_CMD="pyinstaller \
   --collect-submodules=src \
-  --add-data static${ADD_DATA_SEP}static \
-  --add-data src/loeric/loeric_config/performance${ADD_DATA_SEP}loeric/loeric_config/performance \
-  --add-data src/loeric/loeric_config/session${ADD_DATA_SEP}loeric/loeric_config/session \
-  --add-data src/loeric/loeric_config/shell${ADD_DATA_SEP}loeric/loeric_config/shell \
+  --add-data statici:static \
+  --add-data src/loeric/loeric_config/performance:loeric/loeric_config/performance \
+  --add-data src/loeric/loeric_config/session:loeric/loeric_config/session \
+  --add-data src/loeric/loeric_config/shell:loeric/loeric_config/shell \
   --hidden-import mido.backends.rtmidi \
-  -n loeric${EXT} \
+  -n loeric \
   --icon loeric-icon.png \
   -w src/loeric/server/__main__.py"
 
 eval $PYINSTALLER_CMD
 
-echo "Build finished: dist/loeric/loeric${EXT}"
+echo "Build finished."
--- src/loeric/server/__main__.py
+++ src/loeric/server/__main__.py
@@ -1,19 +1,21 @@
-import faulthandler
 import sys
 from pathlib import Path
+
+
+if getattr(sys, "frozen", False):
+    log_file = Path.home() / "loeric.log"
+
+    if sys.stdout is None:
+        sys.stdout = open(log_file, "a")
+
+    if sys.stderr is None:
+        sys.stderr = open(log_file, "a")
+
+
+import faulthandler
 
 from loeric.server.server import start_server
 
 
-log_path = Path.home() / "loeric_error.log"
-
-if sys.stderr is None:
-    sys.stderr = open(log_path, "a")
-
-if sys.stdout is None:
-    sys.stdout = open(log_path, "a")
-
-
 faulthandler.enable()
-
 start_server()
